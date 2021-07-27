ff427df744f5888981911782f811a2c4c919db0d
Oscar Fernando <oscar.cabellos@unmsm.edu.pe>
2021-07-27 17:46:25 -0500

    arreglo menu link doctor

--- src/components/menuClinica/index.jsx
+++ src/components/menuClinica/index.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserAlt, faStickyNote, faCog, faHome } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 export default function MenuClinica({}) {
   const tipo = JSON.parse(sessionStorage.getItem('usuario')).tipoUsuario
+  const [path, setPath] = useState('')
+
+  useEffect(() => {
+    if(tipo === 'paciente') {
+      setPath('/Clinica')
+    }
+    else if(tipo === 'doctor') {
+      setPath('/ClinicaDoctor')
+    }
+    console.log(path)
+  }, [tipo])
   return (
     <>
       <div className="menu-clinica-container">
         <nav className="menu-clinica">
           <ul>
-            <li><Link to='/Clinica'><FontAwesomeIcon icon={faUserAlt}/></Link></li>
+            <li><Link to={path}><FontAwesomeIcon icon={faUserAlt}/></Link></li>
             {
               (tipo === 'paciente')
               ? <li><Link to='/Clinica/Formulario'><FontAwesomeIcon icon={faStickyNote}/></Link></li>
